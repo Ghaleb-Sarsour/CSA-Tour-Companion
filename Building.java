18b91001382aaad265cc066535bbd79b67ecf39e
Ghaleb Sarsour <ghaleb.sarsour2007@gmail.com>
2024-02-21 22:15:19 +0000

    data recovered

---
+++ Building.java
@@ -0,0 +1,19 @@
+public class Building {
+    
+    private int[][] floor1;
+    private int[][] floor2;
+    private int[][] floor3;
+
+    public Building(int[][] floor1, int[][] floor2, int[][] floor3){
+        floor1 = this.floor1;
+        floor2 = this.floor2;
+        floor3 = this.floor3;
+    }
+
+    public void setFloor1(){
+    
+    }
+}
+
+
+
---
+++ mainClass.java
@@ -0,0 +1,10 @@
+public class MainClass {
+    7
+    6     public static void main(String[] args){
+    5         int[][] floor1 = new int[9][6];
+    4         int[][] floor2 = new int[10][7];
+    3         int[][] floor3 = new int[9][5];
+    2
+    1    }
+  9
+    1 }
