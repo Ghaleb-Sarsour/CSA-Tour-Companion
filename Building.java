a8bc5ae6625d85279aa761db0b42b6f21a064e78
Ghaleb Sarsour <ghaleb.sarsour2007@gmail.com>
2024-02-22 15:46:21 +0000

    creating base algorithm to generate floors

--- Building.java
+++ Building.java
@@ -1,18 +1,29 @@
 public class Building {
     
+    //Creating each floor in public class Building {
     private int[][] floor1;
     private int[][] floor2;
     private int[][] floor3;
-
+    
+    //Constructor creating object 
     public Building(int[][] floor1, int[][] floor2, int[][] floor3){
         floor1 = this.floor1;
         floor2 = this.floor2;
         floor3 = this.floor3;
     }
-
-    public void setFloor1(){
     
+    //Setting the rooms of a floor 
+    public void setFloor(int[][] floor, int[] floorRooms){
+        int counter = 0;
+        for(int i = 0; i < floor.length; i++){
+            for(int j = 0; j < floor[i].length; j++){
+                floor[i][j] = floorRooms[counter];
+                counter++;
+            }
+        }
     }
+    
+
 }
 
 
--- mainClass.java
+++ mainClass.java
@@ -1,10 +1,22 @@
-public class MainClass {
-    7
-    6     public static void main(String[] args){
-    5         int[][] floor1 = new int[9][6];
-    4         int[][] floor2 = new int[10][7];
-    3         int[][] floor3 = new int[9][5];
-    2
-    1    }
-  9
-    1 }
+public class mainClass {
+
+    public static void main(String[] args){
+
+        //Creating all 3 floors
+        int[][] floor1 = new int[9][6];
+        int[][] floor2 = new int[10][7];
+        int[][] floor3 = new int[9][5];
+
+        //list of rooms on each floor
+        int[] floor1rooms = {188, 184, 184, 0, 0, 0, 118, 172, 174, 192, 188, 188, 184, 0, 0, 0, 0, 0, 173, 191, 0, 1000, 1000, 1000, 1000, 144, 110, 2000, 113, 0, 189, 187, 0, 181, 145, 123, 121, 0, 0, 152, 0, 0, 0, 0, 147, 144, 0, 0, 0, 158, 0, 0, 0, 0, 147, 146, 0, 0, 0, 158};
+        int[] floor2rooms = {};
+        int[] floor3rooms = {};
+
+        //Creating Object 
+        Building IA = new Building(floor1, floor2, floor3);
+        IA.setFloor(floor1, floor1rooms);
+        int test = floor1[0][0];
+        System.out.println(test);
+
+    }
+}
