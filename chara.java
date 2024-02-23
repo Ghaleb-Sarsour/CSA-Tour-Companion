00f0118c06f5d9f88fed15d4b7eeae7d60992742
Ghaleb Sarsour <ghaleb.sarsour2007@gmail.com>
2024-02-23 16:38:09 +0000

    creating 3rd floor and adding character

---
+++ chara.java
@@ -0,0 +1,27 @@
+public class chara{
+    
+    //Char location 
+    private int xloc;
+    private int yloc;
+    private int zloc;
+
+    //Construtor 
+    public chara(){ 
+        xloc = 0;
+        yloc = 0;
+        zloc = 0;
+        
+    }
+
+    //Getters 
+    public int getXloc(){
+        return xloc;
+    }
+    public int getYloc(){
+        return yloc;
+    }
+    public int getZloc(){
+        return zloc;
+    }
+
+}
